ed3e62a8e82c0217091d02bad3d6bab1a26b7fef
Lenin Meza <merolhack@gmail.com>
2017-11-06 20:47:23 -0600

    :sparkles: feat(gradle): Updated to 2.6

--- android/app/src/main/java/com/lenovo/turns/generated/ExponentBuildConstants.java
+++ android/app/src/main/java/com/lenovo/turns/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package com.lenovo.turns.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "expb930556d942e46989940009ab5ee5e9e://localhost:19000";
 
 }
