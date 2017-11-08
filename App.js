0412ecba7e601709ca515b62eedcdbb0c3646298
Lenin Meza <merolhack@gmail.com>
2017-11-07 22:54:30 -0600

    :sparkles: feat: Grid layout

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Alert, StyleSheet, Text, View, Button } from 'react-native';
+import { Alert, StyleSheet, Text, View, Button, CheckBox } from 'react-native';
+// External dependencies
+import { Col, Row, Grid } from "react-native-easy-grid";
 import SplashScreen from 'react-native-splash-screen';
 
 // We Import our Stylesheet
@@ -9,6 +11,7 @@ export default class App extends Component {
   state = {
     counterG1: 0,
     counterG2: 0,
+    disabled: false,
   }
 
   /**
@@ -17,7 +20,7 @@ export default class App extends Component {
    * @param {string} groupName 
    * @param {string} stateName 
    */
-  setAlertVisible(groupName, stateName) {
+  _setAlertVisible = (groupName, stateName) => {
     const increment = this.state[stateName] + 1;
     this.setState({ [stateName]: increment });
     Alert.alert(
@@ -30,25 +33,88 @@ export default class App extends Component {
     );
   }
 
+  /**
+   * Change the state of the checkbox
+   */
+  _setDisabled = () => {
+    this.setState({disabled: !this.state.disabled});
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <View style={{marginTop: 22}}>
-          <Text style={styles.h1}>Tome su turno</Text>
-        </View>
-        <View style={{marginTop: 22}}>
-          <Button onPress={() => {
-              this.setAlertVisible('G1', 'counterG1')
-            }}
-            title="Grupo 1" />
-        </View>
-        <View style={{marginTop: 22}}>
-          <Button onPress={() => {
-              this.setAlertVisible('G2','counterG2')
-            }}
-            title="Grupo 2" />
-        </View>
-      </View>
+      <Grid>
+        <Row style={{marginTop: 22, marginBottom: 22, height: 40}}>
+          <Col>
+            <Text style={styles.h1}>Tome su turno</Text>
+          </Col>
+          <Col>
+            <Row>
+              <Col style={{ width: 35 }}>
+                <CheckBox
+                  onValueChange={this._setDisabled}
+                  value={this.state.disabled} />
+              </Col>
+              <Col>
+                <Text style={{ marginTop: 5 }}>Discapacitado</Text>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Row style={styles.row}>
+              <Button onPress={() => {
+                  this._setAlertVisible('G1', 'counterG1')
+                }}
+                title="Moto A" />
+            </Row>
+          </Col>
+          <Col>
+            <Row style={styles.row}>
+              <Button onPress={() => {
+                  this._setAlertVisible('G2','counterG2')
+                }}
+                title="Moto B" />
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Row style={styles.row}>
+              <Button onPress={() => {
+                  this._setAlertVisible('G1', 'counterG1')
+                }}
+                title="Moto C" />
+            </Row>
+          </Col>
+          <Col>
+            <Row style={styles.row}>
+              <Button onPress={() => {
+                  this._setAlertVisible('G2','counterG2')
+                }}
+                title="Moto D" />
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Row style={styles.row}>
+              <Button onPress={() => {
+                  this._setAlertVisible('G1', 'counterG1')
+                }}
+                title="Moto E" />
+            </Row>
+          </Col>
+          <Col>
+            <Row style={styles.row}>
+              <Button onPress={() => {
+                  this._setAlertVisible('G2','counterG2')
+                }}
+                title="Moto F" />
+            </Row>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
@@ -63,5 +129,15 @@ const styles = StyleSheet.create({
   h1: {
     fontSize: GeneralStyle.FONT_SIZE_TITLE,
     lineHeight: GeneralStyle.FONT_SIZE_TITLE * 1.5,
+    textAlign: 'center',
   },
+  row: {
+    marginTop: 22,
+    marginRight: 22,
+    marginBottom: 22,
+    marginLeft: 22
+  },
+  btn: {
+    width: '100%',
+  }
 });
